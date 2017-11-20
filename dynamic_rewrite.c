0aba081506fcad142b662f98f5f646e7c344fa62
Daisuke Kondo <gim0kondo@gmail.com>
2017-11-21 02:01:59 +0900

    画面に収まるようにコードを調整

--- dynamic_rewrite.c
+++ dynamic_rewrite.c
@@ -1,17 +1,11 @@
-#include <stdio.h>
-
 typedef int CalcFunc(int);
-
-int main()
-{
+int main() {
     unsigned char code[] = {
-        0x8B, 0x44, 0x24, 0x04,
-        0x83, 0xC0, 0x09, 0xC3
+        0x8B, 0x44, 0x24, 0x04, 0x83, 0xC0/* add */, 0x09, 0xC3
     };
     CalcFunc* pFunc = (CalcFunc*)&code;
     int ret1 = pFunc(3);
     printf("n = %d\n", ret1);
-
     // コードを書き換える
     code[5] = 0xE8; // sub
     int ret2 = pFunc(3);
